9fd6a3288a7a75e0d39e1824b878951a88d6888e
Jose Ignacio Jerez Torrens <joseignaciojereztorrens@192.168.1.9>
2026-04-28 12:23:00 -0400

    pallets enfundados

--- app/controllers/enfundados_controller.rb
+++ app/controllers/enfundados_controller.rb
@@ -19,35 +19,48 @@ class EnfundadosController < ApplicationController
   @enfundados = @enfundados.where("fecha <= ?", @hasta) if @hasta.present?
   @enfundados = @enfundados.order(fecha: :desc, created_at: :desc)
 
-  @total_manual = @enfundados.sum { |e| e.total_manual_reporte }
-  @total_automatica = @enfundados.sum { |e| e.total_automatica_reporte }
+  # Totales que se muestran en la tabla
+  @total_manual = @enfundados.sum { |e| e.total_manual_reporte.to_i }
+  @total_automatica = @enfundados.sum { |e| e.total_automatica_reporte.to_i }
 
+  # Films cambiados según registros de enfundado
   @total_films_manual = @enfundados.sum { |e| e.numero_rollos_films_cambiados_manual.to_i }
   @total_films_automatica = @enfundados.sum { |e| e.numero_rollos_films_cambiados_automatica.to_i }
   @total_films_usados = @total_films_manual + @total_films_automatica
 
-  @total_gramos_manual = @enfundados.sum { |e| e.gramos_consumidos_manual }
-  @total_gramos_automatica = @enfundados.sum { |e| e.gramos_consumidos_automatica }
-  @total_gramos_general = @enfundados.sum { |e| e.gramos_consumidos_total }
+  # Gramos consumidos
+  @total_gramos_manual = @enfundados.sum { |e| e.gramos_consumidos_manual.to_i }
+  @total_gramos_automatica = @enfundados.sum { |e| e.gramos_consumidos_automatica.to_i }
+  @total_gramos_general = @enfundados.sum { |e| e.gramos_consumidos_total.to_i }
 
+  # Rollos entregados por bodega
   @entregas_films = EntregaFilm.all
   @entregas_films = @entregas_films.where("fecha >= ?", @desde) if @desde.present?
   @entregas_films = @entregas_films.where("fecha <= ?", @hasta) if @hasta.present?
 
   @total_films_entregados = @entregas_films.sum(:rollos_entregados).to_i
+
+  # Diferencia entre rollos entregados y films registrados como usados
   @diferencia_films = @total_films_entregados - @total_films_usados
 
+  # Cálculo estimado de rollos según gramos
   @rollos_usados_segun_gramos = @total_gramos_general.to_f / 15000
-@diferencia_rollos_segun_gramos = @total_films_entregados.to_f - @rollos_usados_segun_gramos
+  @diferencia_rollos_segun_gramos = @total_films_entregados.to_f - @rollos_usados_segun_gramos
 
-@total_pallet_manual = @enfundados.sum { |e| e.numero_pallet_enfundado_manual.to_i }
-@total_pallet_automatica = @enfundados.sum { |e| e.numero_pallet_enfundado_automatica.to_i }
-@total_pallet_enfundados = @total_pallet_manual + @total_pallet_automatica
+  # Pallet enfundados: usa los mismos métodos que la tabla
+  @total_pallet_manual = @enfundados.sum { |e| e.total_manual_reporte.to_i }
+  @total_pallet_automatica = @enfundados.sum { |e| e.total_automatica_reporte.to_i }
+  @total_pallet_enfundados = @total_pallet_manual + @total_pallet_automatica
 
-@total_pallet_enf_normal = @enfundados.sum { |e| e.pallet_enf_normal }
-@total_pallet_enf_completo = @enfundados.sum { |e| e.pallet_enf_completo }
-@total_pallet_enf_zuncho = @enfundados.sum { |e| e.pallet_enf_zuncho }
-@total_pallet_enf_calculado = @total_pallet_enf_normal + @total_pallet_enf_completo + @total_pallet_enf_zuncho
+  # Clasificación EP pallet enfundado
+  @total_pallet_enf_normal = @enfundados.sum { |e| e.pallet_enf_normal.to_i }
+  @total_pallet_enf_completo = @enfundados.sum { |e| e.pallet_enf_completo.to_i }
+  @total_pallet_enf_zuncho = @enfundados.sum { |e| e.pallet_enf_zuncho.to_i }
+
+  @total_pallet_enf_calculado =
+    @total_pallet_enf_normal +
+    @total_pallet_enf_completo +
+    @total_pallet_enf_zuncho
 end
 
   # GET /enfundados/new
