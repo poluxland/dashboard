30fdb01acb5fd29e6cc16f63219ef52d40c69046
Jose Ignacio Jerez Torrens <joseignaciojereztorrens@192.168.1.31>
2026-09-04 23:02:33 -0400

    Update maintenance report recipients and Brakeman

--- app/mailers/mantencion_mailer.rb
+++ app/mailers/mantencion_mailer.rb
@@ -3,6 +3,32 @@ class MantencionMailer < ApplicationMailer
     jose.jerez@msindustrial.cl
     julio.alvear@msindustrial.cl
     fernando.gonzalez@msindustrial.cl
+    martin.llancafil@meloncementos.cl
+    daniel.garrido@meloncementos.cl
+    francisco.salgado@meloncementos.cl
+    gerardo.martinez@meloncementos.cl
+    johnny.rute@meloncementos.cl
+    camila.birke@msindustrial.cl
+    juan.belmar@meloncementos.cl
+    manuel.sepulveda@meloncementos.cl
+    alex.rioseco@meloncementos.cl
+    gabriel.arancibia@meloncementos.cl
+    alex.dorante@msindustrial.cl
+    mario.santibanez@meloncementos.cl
+    maximiliano.perez@meloncementos.cl
+    efrain.mindiola@meloncementos.cl
+    hector.ampuero@msindustrial.cl
+    luis.navas@msindustrial.cl
+    isabel.tapia@meloncementos.cl
+    jose.opazo-externo@melonservicios.cl
+    gari.aguilera@meloncementos.cl
+    pia.villegas@meloncementos.cl
+    carolina.vera@meloncementos.cl
+    moira.cisternas@meloncementos.cl
+    exequiel.moya@msindustrial.cl
+    rodrigo.bravo@meloncementos.cl
+    hans.velasquez@msindustrial.cl
+    helmut.brandau@meloncementos.cl
   ].freeze
 
   def weekly_report(reference_date: MantencionWeeklyReport.local_today)
--- test/mailers/mantencion_mailer_test.rb
+++ test/mailers/mantencion_mailer_test.rb
@@ -7,8 +7,35 @@ class MantencionMailerTest < ActionMailer::TestCase
     assert_equal [
       "jose.jerez@msindustrial.cl",
       "julio.alvear@msindustrial.cl",
-      "fernando.gonzalez@msindustrial.cl"
+      "fernando.gonzalez@msindustrial.cl",
+      "martin.llancafil@meloncementos.cl",
+      "daniel.garrido@meloncementos.cl",
+      "francisco.salgado@meloncementos.cl",
+      "gerardo.martinez@meloncementos.cl",
+      "johnny.rute@meloncementos.cl",
+      "camila.birke@msindustrial.cl",
+      "juan.belmar@meloncementos.cl",
+      "manuel.sepulveda@meloncementos.cl",
+      "alex.rioseco@meloncementos.cl",
+      "gabriel.arancibia@meloncementos.cl",
+      "alex.dorante@msindustrial.cl",
+      "mario.santibanez@meloncementos.cl",
+      "maximiliano.perez@meloncementos.cl",
+      "efrain.mindiola@meloncementos.cl",
+      "hector.ampuero@msindustrial.cl",
+      "luis.navas@msindustrial.cl",
+      "isabel.tapia@meloncementos.cl",
+      "jose.opazo-externo@melonservicios.cl",
+      "gari.aguilera@meloncementos.cl",
+      "pia.villegas@meloncementos.cl",
+      "carolina.vera@meloncementos.cl",
+      "moira.cisternas@meloncementos.cl",
+      "exequiel.moya@msindustrial.cl",
+      "rodrigo.bravo@meloncementos.cl",
+      "hans.velasquez@msindustrial.cl",
+      "helmut.brandau@meloncementos.cl"
     ], email.to
+    assert_equal 29, email.to.uniq.size
     assert_equal [ "control@msindustrial.cl" ], email.from
     assert_equal "Informe semanal de mantenciones · Semana 36", email.subject
     assert_match "Mantenciones · Semana 36", email.html_part.body.to_s
